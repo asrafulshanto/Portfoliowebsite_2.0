f68bddcd0e05191efec9ad9a666228edf6da6219
Asraful Shanto <64688056+asrafulshanto@users.noreply.github.com>
2024-02-08 04:04:51 +0600

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -88,10 +88,10 @@ https://fontawesome.com/icons?d=gallery */
       skillName: "nodejs",
       fontAwesomeClassname: "fab fa-node"
     },
-    {
+   /* {
       skillName: "swift",
       fontAwesomeClassname: "fab fa-swift"
-    },
+    }, */
     {
       skillName: "npm",
       fontAwesomeClassname: "fab fa-npm"
@@ -103,19 +103,19 @@ https://fontawesome.com/icons?d=gallery */
     {
       skillName: "aws",
       fontAwesomeClassname: "fab fa-aws"
-    },
-    {
+    }, 
+ /* {
       skillName: "firebase",
       fontAwesomeClassname: "fas fa-fire"
-    },
+    },*/
     {
       skillName: "python",
       fontAwesomeClassname: "fab fa-python"
     },
-    {
+/*  {
       skillName: "docker",
       fontAwesomeClassname: "fab fa-docker"
-    }
+    } */
   ],
   display: true // Set false to hide this section, defaults to true
 };
