158225594779e40d022212db7452cf7ff1f4a94c
Asraful Shanto <64688056+asrafulshanto@users.noreply.github.com>
2024-02-08 02:49:05 +0600

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -236,7 +236,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
